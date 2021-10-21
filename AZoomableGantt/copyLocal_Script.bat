74ce151784a61012b69c78055ed7ba6237fcd976
RobjSky <bjoern.mueller@posteo.de>
2021-10-21 13:46:32 +0200

    AZoomableGantt delete

--- AZoomableGantt/copyLocal_Script.bat
+++
@@ -1,9 +0,0 @@
-set "source=u:\MyDocuments\00_MyStuff\MSTR Visualizations\Development\AZoomableGantt\AZoomableGantt"
-
-set "destination=c:\Program Files\MicroStrategy\Workstation\Code\plugins\AZoomableGantt"
-
-robocopy /mir "%source%" "%destination%"
-
-start "C:\Program Files\MicroStrategy\Workstation\Workstation.exe" "u:\MyDocuments\00_MyStuff\MSTR Visualizations\Development\AZoomableGantt\DemoZoomableGantt.mstr"
-
-exit /b
